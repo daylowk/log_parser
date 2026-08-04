0055a8988dc9a6cb0c5a5e0ed8981c54833d1d28
Daylon <d3f4l7gt5@gmail.com>
2026-08-04 12:49:58 -0300

    feat: ip extraction and counting

--- parsers/openssh_parser.py
+++ parsers/openssh_parser.py
@@ -1,21 +1,36 @@
 import re
+import ipaddress
 
 timestamp_pattern = re.compile(r'\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}')
-
+ip_pattern = re.compile(r'\b(?:\d{1,3}\.){3}\d{1,3}\b')
 total = 0
 first_timestamp = None
+ips = {}
 
 with open('log_samples/OpenSSH_2k.log', 'r') as arquivo:
     for linha in arquivo:
         total += 1
+        timestamp_match = timestamp_pattern.search(linha)
+        ip_match = ip_pattern.search(linha)
 
-        timestamp = timestamp_pattern.search(linha).group()
+        if timestamp_match:
+            timestamp = timestamp_match.group()
+            if first_timestamp is None:
+                first_timestamp = timestamp          
+            last_timestamp = timestamp
 
-        if first_timestamp is None:
-            first_timestamp = timestamp
-            
-        last_timestamp = timestamp
+        if ip_match:
+            ip = ip_match.group()
+            try:
+                ipaddress.IPv4Address(ip)
+                ips[ip] = ips.get(ip, 0) + 1
+            except ipaddress.AddressValueError:
+                continue
+        else:
+            ips['Sem IP'] = ips.get('Sem IP', 0) + 1
 
 print(f'Total de linhas: {total}')
 print(f'Primeiro timestamp: {first_timestamp}')
 print(f'Último timestamp: {last_timestamp}')
+for ip, counter in ips.items():
+    print(f'{ip}: {counter}')
