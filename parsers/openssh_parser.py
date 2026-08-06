24aca0529191d983fd47ec51eef7d8ff35e8093f
Daylon <d3f4l7gt5@gmail.com>
2026-08-06 10:57:40 -0300

    feat: user extraction and some adjusts

--- parsers/openssh_parser.py
+++ parsers/openssh_parser.py
@@ -1,17 +1,37 @@
 import re
 import ipaddress
 
-timestamp_pattern = re.compile(r'\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}')
-ip_pattern = re.compile(r'\b(?:\d{1,3}\.){3}\d{1,3}\b')
+TIMESTAMP_PATTERN = re.compile(r'\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}')
+IP_PATTERN = re.compile(r'\b(?:\d{1,3}\.){3}\d{1,3}\b')
+
+USER_PATTERNS = {
+    'accepted_password': re.compile(r'(?<=Accepted\spassword\sfor\s)\S+'),
+    'too_many_auth_failures': re.compile(r'(?<=Disconnecting:\sToo\smany\sauthentication\sfailures\sfor\s)\S+'),
+    'failed_password': re.compile(r'(?<=Failed\spassword\sfor\s)\S+(?=\sfrom)'),
+    'invalid_user': re.compile(r'(?<=Invalid\suser\s)\S+', re.IGNORECASE),
+    'user_equal': re.compile(r'(?<=user=)\S+'),
+    'session_closed': re.compile(r'(?<=session\s(?:closed|opened)\sfor\suser\s)\S+')
+}
+
+def user_capture(log_line):
+    for pattern in USER_PATTERNS.values():
+        match = pattern.search(log_line)
+        if match:
+            return match.group()
+
+
 total = 0
 first_timestamp = None
+last_timestamp = None
 ips = {}
+lines_without_ip = 0
+users = {}
 
-with open('log_samples/OpenSSH_2k.log', 'r') as arquivo:
-    for linha in arquivo:
+with open('log_samples/OpenSSH_2k.log', 'r') as log_file:
+    for line in log_file:
         total += 1
-        timestamp_match = timestamp_pattern.search(linha)
-        ip_match = ip_pattern.search(linha)
+        timestamp_match = TIMESTAMP_PATTERN.search(line)
+        ip_match = IP_PATTERN.search(line)
 
         if timestamp_match:
             timestamp = timestamp_match.group()
@@ -27,10 +47,15 @@ with open('log_samples/OpenSSH_2k.log', 'r') as arquivo:
             except ipaddress.AddressValueError:
                 continue
         else:
-            ips['Sem IP'] = ips.get('Sem IP', 0) + 1
+            lines_without_ip += 1
 
-print(f'Total de linhas: {total}')
-print(f'Primeiro timestamp: {first_timestamp}')
-print(f'Último timestamp: {last_timestamp}')
+        user = user_capture(line)
+        users[user] = users.get(user, 0) + 1
+
+print(f'Total of lines: {total}')
+print(f'First timestamp: {first_timestamp}')
+print(f'Last timestamp: {last_timestamp}')
 for ip, counter in ips.items():
     print(f'{ip}: {counter}')
+print(f'Lines without IP: {lines_without_ip}')
+print(users)
